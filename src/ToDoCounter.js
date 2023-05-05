f7544040ac2e051da74ede2296c2b7fff1174962
Guarnerman <126298667+Guarnerman@users.noreply.github.com>
2023-05-05 16:55:37 -0600

    Sesión 1
    Importar/exportar elementos

---
+++ src/ToDoCounter.js
@@ -0,0 +1,11 @@
+
+
+function ToDoCounter() {
+    return(
+      <h1>
+        Has completado 3 de 5 ToDo's
+      </h1>
+    );
+  }
+
+  export { ToDoCounter };
---
+++ src/ToDoItem.js
@@ -0,0 +1,11 @@
+function ToDoItem() {
+    return(
+      <li>
+        <span>V</span>
+        <p>Llorar por la vida</p>
+        <span>X</span>
+      </li>
+    );
+  }
+
+  export { ToDoItem };
